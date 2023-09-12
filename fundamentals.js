cc6a0dc912cb4b55f28cb055702e0a3176771e52
张时进 <1364226740@qq.com>
2023-09-12 18:15:17 +0800

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -1,18 +1,39 @@
 const aCar = {
-  owner : "Joe Bloggs",
+  owner: "Joe Bloggs",
   type: {
-    make: 'Toyota',
-    model: 'Corolla',
-    cc: 1.8
+    make: "Toyota",
+    model: "Corolla",
+    cc: 1.8,
   },
-  registration : {
+  registration: {
     year: 201,
-    countyCode: 'WD',
-    number: 1058
-  }
+    countyCode: "WD",
+    number: 1058,
+  },
 };
 
-console.log(aCar.owner + ' drives a ' + aCar.type.make)
+// Add a new property
+aCar.mileage = 10000;
+// Add a new object
+aCar.color = {
+  exterior: "red",
+  interior: {
+    texture: "leather",
+    shade: "cream",
+  },
+};
+console.log(
+  "It is a " +
+    aCar.color.exterior +
+    " car, " +
+    aCar.mileage +
+    " mileage, with " +
+    aCar.color.interior.texture +
+    " interior."
+);
+
+console.log(aCar.owner + " drives a " + aCar.type.make);
+
 console.log(
   "Reg. = " +
     aCar.registration.year +
